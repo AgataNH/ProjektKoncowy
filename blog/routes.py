09784624e1b0c910d9f08f6bd11a5f3daa8a4b52
Agata Niedolistek <a.niedolistek@ibe.edu.pl>
2022-07-22 22:43:27 +0200

    refactoring

--- blog/routes.py
+++ blog/routes.py
@@ -1,6 +1,33 @@
-from flask import render_template
+from flask import render_template, request
 from blog import app
+from blog.models import Entry, db
+from blog.forms import EntryForm
 
 @app.route("/")
 def index():
-   return render_template("base.html")
+    all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc())
+
+    return render_template("homepage.html", all_posts=all_posts)
+
+@app.route("/new-post/", methods=["GET", "POST"])
+@app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
+def edit_entry(entry_id=None):
+    form = EntryForm()
+    errors = None
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            entry = Entry(
+                title=form.title.data,
+                body=form.body.data,
+                is_published=form.is_published.data
+            )
+            db.session.add(entry)
+            db.session.commit()
+            if entry_id:
+                entry = Entry.query.filter_by(id=entry_id).first_or_404()
+                form = EntryForm(obj=entry)
+                form.populate_obj(entry)
+                db.session.commit()
+        else:
+           errors = form.errors
+    return render_template("entry_form.html", form=form, errors=errors)
