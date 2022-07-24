e788ef3b2d0d16417a599ceddc8a090aad313171
Agata Niedolistek <a.niedolistek@ibe.edu.pl>
2022-07-24 10:47:54 +0200

    refactoring2

--- blog/routes.py
+++ blog/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 from blog import app
 from blog.models import Entry, db
 from blog.forms import EntryForm
@@ -10,24 +10,35 @@ def index():
     return render_template("homepage.html", all_posts=all_posts)
 
 @app.route("/new-post/", methods=["GET", "POST"])
+def create_entry():
+    return create_or_edit(entry_id=None)
+
 @app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
-def edit_entry(entry_id=None):
-    form = EntryForm()
-    errors = None
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            entry = Entry(
+def edit_entry(entry_id):
+   return create_or_edit(entry_id=entry_id)
+
+def create_or_edit(entry_id):
+    if entry_id==None:
+        form = EntryForm()
+        errors = None
+        if request.method == 'POST':
+            if form.validate_on_submit():
+                entry = Entry(
                 title=form.title.data,
                 body=form.body.data,
                 is_published=form.is_published.data
             )
             db.session.add(entry)
             db.session.commit()
-            if entry_id:
-                entry = Entry.query.filter_by(id=entry_id).first_or_404()
-                form = EntryForm(obj=entry)
+    elif entry_id:
+        entry = Entry.query.filter_by(id=entry_id).first_or_404()
+        form = EntryForm(obj=entry)
+        errors = None
+        if request.method == 'POST':
+            if form.validate_on_submit():
                 form.populate_obj(entry)
                 db.session.commit()
-        else:
-           errors = form.errors
+    else:
+        errors = form.errors
+        return redirect(url_for('index'))
     return render_template("entry_form.html", form=form, errors=errors)
